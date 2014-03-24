0029d8aa5a1afa4e266b6d26af959a17044daee0
Snarky <thesnarky1@users.noreply.github.com>
2014-03-24 17:54:21 -0400

    minor tweaks

--- js/coordinate.js
+++ js/coordinate.js
@@ -1,22 +1,22 @@
 var Coordinate = function(x,y) {
-  this._x = x;
-  this._y = y;
+  this.x = x;
+  this.y = y;
 }
 
 Coordinate.prototype.toString = function() {
-  return this._x + "," + this._y;
+  return this.x + "," + this.y;
 }
 
 Coordinate.prototype.fromString = function(toParse) {
   var parts = toParse.split(",");
-  this._x = Number(parts[0]);
-  this._y = Number(parts[1]);
+  this.x = Number(parts[0]);
+  this.y = Number(parts[1]);
 }
 
 Coordinate.prototype.getX = function() {
-  return this._x;
+  return this.x;
 }
 
 Coordinate.prototype.getY = function() {
-  return this._y;
+  return this.y;
 }
