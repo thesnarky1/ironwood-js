118e696eac48d61f021e39cb7f3b7c7ffdf79861
Snarky <thesnarky1@users.noreply.github.com>
2014-03-25 12:50:24 -0400

    initial creation of items class

---
+++ js/items.js
@@ -0,0 +1,36 @@
+var Items = function() {
+  this._items = [];
+}
+
+Items.prototype.getItems = function() {
+  return this._items;
+}
+
+Items.prototype.trapdoorAt = function(coords) {
+  var item = this.itemAt(coords);
+  return (item && item instanceof Trapdoor);
+}
+
+Items.prototype.itemAt = function(coords) {
+  for(item in this._items) {
+    if(item.getCoords() == coords) {
+      return item;
+    }
+  }
+  return false;
+}
+
+Items.prototype.bodyNearPlayer = function(player) {
+  var playerCoords = player.getCoords();
+  for(item in this._items) {
+    var itemCoords = item.getCoords();
+    if(!(item instanceof Body)) {
+      //Skip, we only want bodies
+    } else if(itemCoords == playerCoords) {
+      //Skip, we don't want bodies under the player
+    } else if(itemCoords.withinRadius(playerCoords, 1, true)) {
+      return item;
+    }
+  }
+  return false;
+}
