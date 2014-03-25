9348185c14ac46dd8376702fba20a4dc1e9999db
Snarky <thesnarky1@users.noreply.github.com>
2014-03-25 13:08:32 -0400

    initial creation of Gametime class

---
+++ js/game_time.js
@@ -0,0 +1,15 @@
+var GameTime = function() {
+  this._tick = 0;
+}
+
+GameTime.prototype.getTick() = function() {
+  return this._tick;
+}
+
+GameTime.prototype.advance = function() {
+  return this._tick++;
+}
+
+GameTime.prototype.previous = function() {
+  return this._tick--;
+}
