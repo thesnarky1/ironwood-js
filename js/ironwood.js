d740d121d45a42b1a752d81b68d6410a02ae813c
Snarky <thesnarky1@users.noreply.github.com>
2014-04-01 19:29:27 -0400

    Added loaded to front page, game starts on keypress. Also added credits down bottom.

--- js/ironwood.js
+++ js/ironwood.js
@@ -3,23 +3,32 @@ var Ironwood = {
   game: null,
 
   init: function() { //Init the required variables
-    var displayOptions = {
-      width: IRONWOOD_WIDTH,
-      height: IRONWOOD_HEIGHT,
-      spacing: .9,
-      fontFamily: "arial"
-    }
-    this.display = new ROT.Display(displayOptions);
-    document.body.appendChild(this.display.getContainer());
-
-    //Make the game
-    this.game = new Game(this.WIDTH, this.HEIGHT);
-
-    //Start this game's scheduler and engine (not in Ironwood so we can restart the game)
-    this.scheduler = new ROT.Scheduler.Simple();
-    this.scheduler.add(this.game.getPlayer(), true);
-    this.engine = new ROT.Engine(this.scheduler);
-    this.engine.start();
+    window.addEventListener("keypress", this);
   },
+
+  handleEvent: function(e) {
+    if(e.type == "keypress") { //Load the game
+      window.removeEventListener("keypress", this);
+      var displayOptions = {
+        width: IRONWOOD_WIDTH,
+        height: IRONWOOD_HEIGHT,
+        spacing: .9,
+        fontFamily: "arial"
+      }
+      this.display = new ROT.Display(displayOptions);
+      document.getElementById('content').innerHTML = "";
+      document.getElementById('content').appendChild(this.display.getContainer());
+
+      //Make the game
+      this.game = new Game(this.WIDTH, this.HEIGHT);
+
+      //Start this game's scheduler and engine (not in Ironwood so we can restart the game)
+      this.scheduler = new ROT.Scheduler.Simple();
+      this.scheduler.add(this.game.getPlayer(), true);
+      this.engine = new ROT.Engine(this.scheduler);
+      this.engine.start();
+    }
+  }
+
 
 }
